244dedf19881e7c40c2e2072af5c323ae2b77ad1
Vikash Kumar <vikash.kumar@cariboutech.com>
2020-06-04 14:05:36 +0530

    Added few images for contact.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $('#pagepiling').pagepiling({
       menu: '#menu',
       anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-      sectionsColor: ['black', 'white', '#2C3E50', '#39C', 'white'],
+      sectionsColor: ['black', 'white', '#2C3E50', '#39C', '#C1CBE1'],
       navigation: {
         'position': 'right',
         'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5']
